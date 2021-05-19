72bb6e53a40b6b88d64c63a75304d3b6752e9d95
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-19 16:55:17 +0800

    [example] update example

--- examples/sci4sif.py
+++ examples/sci4sif.py
@@ -3,10 +3,16 @@
 
 from EduNLP.SIF import sif4sci
 
-item = r"集合$A={x \in R | |x - 2| \leq 5}$中最小整数位$\SIFBlank$"
-# print(sif4sci(item, symbol="t", tokenization=False))
+item = r"若集合$A=\{x \in R | |x - 2| \leq 5\}$，则$A$中最小整数位是$\SIFBlank$"
+print(item)
+print(sif4sci(item, symbol="t", tokenization=False))
 print(sif4sci(item, symbol="t"))
-# print(sif4sci(item, symbol="fgm", tokenization=False))
+print(sif4sci(item, symbol="fgm", tokenization=False))
 print(sif4sci(item, symbol="fgm"))
 print(sif4sci(item, symbol="gm", tokenization_params={"formula_params": {"method": "ast"}}))
-print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast"}}))
+print(sif4sci(item, symbol="gm", tokenization_params={"formula_params": {"method": "linear"}}))
+print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "ord2token": True}}))
+print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "ord2token": True, "var_numbering": True}}))
+print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "return_type": "list"}}))
+print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "return_type": "ast"}}))
+print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "return_type": "formula"}}))
