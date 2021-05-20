35e1d2d0d1c158594a16e8a9ce48d5708db31ef1
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-20 22:11:02 +0800

    [test] simplify codes based on the configuration

--- tests/test_sif/test_segement.py
+++ tests/test_sif/test_segement.py
@@ -3,16 +3,11 @@
 
 import pytest
 
-import os
-from PIL import Image
 from EduNLP.SIF.segment import seg
-from EduNLP.utils import abs_current_dir, path_append, image2base64
+from EduNLP.utils import image2base64
 
 
-def test_segment():
-    img_dir = os.path.abspath(path_append(abs_current_dir(__file__), "..", "..", "asset", "_static"))
-    figure0 = Image.open(path_append(img_dir, "item_formula.png", to_str=True))
-    figure1 = Image.open(path_append(img_dir, "item_figure.png", to_str=True))
+def test_segment(figure0, figure1, figure0_base64, figure1_base64):
     seg(
         r"如图所示，则$\FormFigureID{0}$的面积是$\SIFBlank$。$\FigureID{1}$",
         figures={
@@ -20,9 +15,6 @@ def test_segment():
             "1": figure1
         }
     )
-
-    figure0_base64 = image2base64(figure0)
-    figure1_base64 = image2base64(figure1)
     s = seg(
         r"如图所示，则$\FormFigureBase64{%s}$的面积是$\SIFBlank$。$\FigureBase64{%s}$" % (figure0_base64, figure1_base64),
         figures=True
