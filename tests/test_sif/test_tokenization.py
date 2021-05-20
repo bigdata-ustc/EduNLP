18bc3ac267287551e7e0db55b2e0296ffbbdeae6
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-20 22:10:18 +0800

    [test] for tokenization

--- tests/test_sif/test_tokenization.py
+++ tests/test_sif/test_tokenization.py
@@ -2,8 +2,11 @@
 # 2021/5/20 @ tongshiwei
 
 import pytest
+from EduNLP.SIF.constants import Symbol
+from EduNLP.SIF.segment.segment import SegmentList
 from EduNLP.SIF.tokenization import text
 from EduNLP.SIF.tokenization import formula
+from EduNLP.SIF.tokenization.tokenization import TokenList
 
 
 def test_text_tokenization():
@@ -20,3 +23,12 @@ def test_formula_tokenization():
 
     with pytest.raises(TypeError):
         formula.tokenize(r"\phantom{=}56+4", method="ast")
+
+
+def test_tokenization():
+    tl = TokenList(SegmentList(""))
+    with pytest.raises(TypeError):
+        tl.append(Symbol("[Unknown]"))
+
+    with pytest.raises(TypeError):
+        tl.append("[Unknown]")
