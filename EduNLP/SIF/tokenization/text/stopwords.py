f0a0f7726640782266813a04d82785c0ed9e9688
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-20 15:58:50 +0800

    [feat] enable stopwords filtering

--- EduNLP/SIF/tokenization/text/stopwords.py
+++ EduNLP/SIF/tokenization/text/stopwords.py
@@ -1,2 +1,21 @@
 # coding: utf-8
 # 2021/5/18 @ tongshiwei
+
+import os
+from EduNLP.utils import abs_current_dir, path_append
+
+DEFAULT_FILEPATH = os.path.abspath(
+    path_append(abs_current_dir(__file__), "..", "..", "..", "..", "meta_data", "sif_stopwords.txt")
+)
+
+
+def get_stopwords(filepath=DEFAULT_FILEPATH):
+    _stopwords = set()
+    with open(filepath) as f:
+        for line in f:
+            _stopwords.add(line.strip())
+
+    return _stopwords
+
+
+DEFAULT_STOPWORDS = get_stopwords()
--- EduNLP/SIF/tokenization/text/tokenization.py
+++ EduNLP/SIF/tokenization/text/tokenization.py
@@ -1,9 +1,13 @@
 # coding: utf-8
 # 2021/5/18 @ tongshiwei
+import logging
 import jieba
+from .stopwords import DEFAULT_STOPWORDS
+
+jieba.setLogLevel(logging.INFO)
 
 
-def tokenize(text, granularity="word", stopwords=None):
+def tokenize(text, granularity="word", stopwords=DEFAULT_STOPWORDS):
     stopwords = stopwords if stopwords is not None else {}
     if granularity == "word":
         return [token for token in jieba.cut(text) if token not in stopwords]
