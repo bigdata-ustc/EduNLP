31177c81fe57b24819ecac99f682e7edcffea060
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-20 16:12:17 +0800

    [feat] enable figure linking [docs] add annotations

--- EduNLP/SIF/sif.py
+++ EduNLP/SIF/sif.py
@@ -13,12 +13,36 @@ def to_sif(item):
     return item
 
 
-def sif4sci(item: str, figures: dict = None, safe=True, errors="raise", symbol: str = None, tokenization=True,
+def sif4sci(item: str, figures: dict = None, safe=True, symbol: str = None, tokenization=True,
             tokenization_params=None):
+    """
+
+    Default to use linear Tokenizer, change the tokenizer by specifying tokenization_params
+
+    Parameters
+    ----------
+    item
+    figures
+    safe
+    symbol
+    tokenization
+    tokenization_params:
+        method: which tokenizer to be used, "linear" or "ast"
+        The parameters only useful for "linear":
+
+        The parameters only useful for "ast":
+            ord2token: whether to transfer the variables (mathord) and constants (textord) to special tokens.
+            var_numbering: whether to use number suffix to denote different variables
+
+    Returns
+    -------
+    When tokenization is False, return SegmentList;
+    When tokenization is True, return TokenList
+    """
     if safe is True and is_sif(item) is not True:
         item = to_sif(item)
 
-    ret = seg(item, symbol)
+    ret = seg(item, figures, symbol)
 
     if tokenization:
         ret = tokenize(ret, **(tokenization_params if tokenization_params is not None else {}))
