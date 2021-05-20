a181596bdf819c7492afdb8ed4ba7736eb193a8c
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-20 22:12:20 +0800

    [docs] add examples [feat] tokenization as bool

--- EduNLP/SIF/sif.py
+++ EduNLP/SIF/sif.py
@@ -13,7 +13,7 @@ def to_sif(item):
     return item
 
 
-def sif4sci(item: str, figures: dict = None, safe=True, symbol: str = None, tokenization=True,
+def sif4sci(item: str, figures: (dict, bool) = None, safe=True, symbol: str = None, tokenization=True,
             tokenization_params=None):
     """
 
@@ -42,19 +42,31 @@ def sif4sci(item: str, figures: dict = None, safe=True, symbol: str = None, toke
     Examples
     --------
     >>> test_item = r"如图所示，则$\\bigtriangleup ABC$的面积是$\\SIFBlank$。$\\FigureID{1}$"
-    >>> sif4sci(test_item)
+    >>> tl = sif4sci(test_item)
+    >>> tl
     ['如图所示', '\\\\bigtriangleup', 'ABC', '面积', '\\\\SIFBlank', \\FigureID{1}]
+    >>> tl.text_tokens
+    ['如图所示', '面积']
+    >>> tl.formula_tokens
+    ['\\\\bigtriangleup', 'ABC']
+    >>> tl.figure_tokens
+    [\\FigureID{1}]
+    >>> tl.ques_mark_tokens
+    ['\\\\SIFBlank']
     >>> sif4sci(test_item, symbol="gm", tokenization_params={"formula_params": {"method": "ast"}})
     ['如图所示', <Formula: \\bigtriangleup ABC>, '面积', '[MARK]', '[FIGURE]']
     >>> sif4sci(test_item, symbol="tfgm")
     ['[TEXT]', '[FORMULA]', '[TEXT]', '[MARK]', '[TEXT]', '[FIGURE]']
+    >>> sif4sci(test_item, symbol="gm",
+    ... tokenization_params={"formula_params": {"method": "ast", "return_type": "list"}})
+    ['如图所示', '\\\\bigtriangleup', 'A', 'B', 'C', '面积', '[MARK]', '[FIGURE]']
     """
     if safe is True and is_sif(item) is not True:
         item = to_sif(item)
 
     ret = seg(item, figures, symbol)
 
-    if tokenization:
+    if tokenization is True:
         ret = tokenize(ret, **(tokenization_params if tokenization_params is not None else {}))
 
     return ret
