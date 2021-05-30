ce9a2cc4ea5344c061dbc29f5f962da2f63bcbb4
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-30 18:07:20 +0800

    [feat] Annotation -> Tag

--- EduNLP/SIF/segment/segment.py
+++ EduNLP/SIF/segment/segment.py
@@ -67,6 +67,10 @@ class QuesMarkSegment(str):
     pass
 
 
+class TagSegment(str):
+    pass
+
+
 class SegmentList(object):
     def __init__(self, item, figures: dict = None):
         self._segments = []
@@ -74,6 +78,7 @@ class SegmentList(object):
         self._formula_segments = []
         self._figure_segments = []
         self._ques_mark_segments = []
+        self._tag_segments = []
         segments = re.split(r"(\$.+?\$)", item)
         for segment in segments:
             if not segment:
@@ -90,6 +95,8 @@ class SegmentList(object):
                 self.append(FigureSegment(segment[1:-1], is_base64=True, figure_instance=figures))
             elif re.match(r"\$\\(SIFBlank|SIFChoice)\$", segment):
                 self.append(QuesMarkSegment(segment[1:-1]))
+            elif re.match(r"\$\\SIFTag\{.+?}\$", segment):
+                self.append(TagSegment(segment[1:-1]))
             else:
                 self.append(LatexFormulaSegment(segment[1:-1]))
         self._seg_idx = None
@@ -109,6 +116,8 @@ class SegmentList(object):
             self._figure_segments.append(len(self))
         elif isinstance(segment, QuesMarkSegment):
             self._ques_mark_segments.append(len(self))
+        elif isinstance(segment, TagSegment):
+            self._tag_segments.append(len(self))
         else:
             raise TypeError("Unknown Segment Type: %s" % type(segment))
         self._segments.append(segment)
@@ -135,6 +144,10 @@ class SegmentList(object):
     @property
     def ques_mark_segments(self):
         return [self._segments[i] for i in self._ques_mark_segments]
+
+    @property
+    def tag_segments(self):
+        return [self._segments[i] for i in self._tag_segments]
 
     def to_symbol(self, idx, symbol):
         self._segments[idx] = symbol
@@ -171,7 +184,7 @@ class SegmentList(object):
     def filter(self, drop: (set, str) = "", keep: (set, str) = "*"):
         _drop = {c for c in drop} if isinstance(drop, str) else drop
         if keep == "*":
-            _keep = {c for c in "tfgm" if c not in _drop}
+            _keep = {c for c in "tfgma" if c not in _drop}
         else:
             _keep = {c for c in keep if c not in _drop} if isinstance(keep, str) else keep
         self._seg_idx = set()
@@ -183,6 +196,8 @@ class SegmentList(object):
             self._seg_idx |= set(self._figure_segments)
         if "m" in _keep:
             self._seg_idx |= set(self._ques_mark_segments)
+        if "a" in _keep:
+            self._seg_idx |= set(self._tag_segments)
         yield
         self._seg_idx = None
 
@@ -196,7 +211,7 @@ class SegmentList(object):
 
 
 def seg(item, figures=None, symbol=None):
-    """
+    r"""
 
     Parameters
     ----------
@@ -209,37 +224,57 @@ def seg(item, figures=None, symbol=None):
 
     Examples
     --------
-    >>> test_item = r"如图所示，则$\\bigtriangleup ABC$的面积是$\\SIFBlank$。$\\FigureID{1}$"
+    >>> test_item = r"如图所示，则$\bigtriangleup ABC$的面积是$\SIFBlank$。$\FigureID{1}$"
     >>> s = seg(test_item)
     >>> s
-    ['如图所示，则', '\\\\bigtriangleup ABC', '的面积是', '\\\\SIFBlank', '。', \\FigureID{1}]
+    ['如图所示，则', '\\bigtriangleup ABC', '的面积是', '\\SIFBlank', '。', \FigureID{1}]
     >>> s.describe()
     {'t': 3, 'f': 1, 'g': 1, 'm': 1}
     >>> with s.filter("f"):
     ...     s
-    ['如图所示，则', '的面积是', '\\\\SIFBlank', '。', \\FigureID{1}]
+    ['如图所示，则', '的面积是', '\\SIFBlank', '。', \FigureID{1}]
     >>> with s.filter(keep="t"):
     ...     s
     ['如图所示，则', '的面积是', '。']
     >>> with s.filter():
     ...     s
-    ['如图所示，则', '\\\\bigtriangleup ABC', '的面积是', '\\\\SIFBlank', '。', \\FigureID{1}]
+    ['如图所示，则', '\\bigtriangleup ABC', '的面积是', '\\SIFBlank', '。', \FigureID{1}]
     >>> seg(test_item, symbol="fgm")
     ['如图所示，则', '[FORMULA]', '的面积是', '[MARK]', '。', '[FIGURE]']
     >>> seg(test_item, symbol="tfgm")
     ['[TEXT]', '[FORMULA]', '[TEXT]', '[MARK]', '[TEXT]', '[FIGURE]']
-    >>> seg(r"如图所示，则$\\FormFigureID{0}$的面积是$\\SIFBlank$。$\\FigureID{1}$")
-    ['如图所示，则', \\FormFigureID{0}, '的面积是', '\\\\SIFBlank', '。', \\FigureID{1}]
-    >>> seg(r"如图所示，则$\\FormFigureID{0}$的面积是$\\SIFBlank$。$\\FigureID{1}$", symbol="fgm")
+    >>> seg(r"如图所示，则$\FormFigureID{0}$的面积是$\SIFBlank$。$\FigureID{1}$")
+    ['如图所示，则', \FormFigureID{0}, '的面积是', '\\SIFBlank', '。', \FigureID{1}]
+    >>> seg(r"如图所示，则$\FormFigureID{0}$的面积是$\SIFBlank$。$\FigureID{1}$", symbol="fgm")
     ['如图所示，则', '[FORMULA]', '的面积是', '[MARK]', '。', '[FIGURE]']
     >>> s.text_segments
     ['如图所示，则', '的面积是', '。']
     >>> s.formula_segments
-    ['\\\\bigtriangleup ABC']
+    ['\\bigtriangleup ABC']
     >>> s.figure_segments
-    [\\FigureID{1}]
+    [\FigureID{1}]
     >>> s.ques_mark_segments
-    ['\\\\SIFBlank']
+    ['\\SIFBlank']
+    >>> test_item_1 = {
+    ...     "stem": r"若复数$z=1+2 i+i^{3}$，则$|z|=$",
+    ...     "options": ['0', '1', '$\sqrt{2}$', '2']
+    ... }
+    >>> from EduNLP.utils import dict2str4sif
+    >>> test_item_1_str = dict2str4sif(test_item_1)
+    >>> test_item_1_str  # doctest: +ELLIPSIS
+    '$\\SIFAnn{stem_begin}$...$\\SIFAnn{stem_end}$$\\SIFAnn{options_begin}$$\\SIFAnn{list_0}$0...$\\SIFAnn{options_end}$'
+    >>> s1 = seg(test_item_1_str, symbol="tfgm")
+    >>> s1  # doctest: +ELLIPSIS
+    ['\\SIFAnn{stem_begin}'...'\\SIFAnn{stem_end}', '\\SIFAnn{options_begin}', '\\SIFAnn{list_0}', ...]
+    >>> with s1.filter(keep="a"):
+    ...     s1  # doctest: +ELLIPSIS
+    [...'\\SIFAnn{list_0}', '\\SIFAnn{list_1}', '\\SIFAnn{list_2}', '\\SIFAnn{list_3}', '\\SIFAnn{options_end}']
+    >>> s1.tag_segments  # doctest: +ELLIPSIS
+    ['\\SIFAnn{stem_begin}', '\\SIFAnn{stem_end}', '\\SIFAnn{options_begin}', ... '\\SIFAnn{options_end}']
+    >>> test_item_1_str_2 = dict2str4sif(test_item_1, tag_mode="head", add_list_no_tag=False)
+    >>> s2 = seg(test_item_1_str_2, symbol="fgm")
+    >>> s2.tag_segments
+    ['\\SIFAnn{stem}', '\\SIFAnn{options}']
     """
     segments = SegmentList(item, figures)
     if symbol is not None:
