41cc4c74d24413c64e5b66236b1864b4fea8de48
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-30 18:03:54 +0800

    [docs] update example

--- examples/sif/sci4sif.py
+++ examples/sif/sci4sif.py
@@ -26,7 +26,7 @@ from EduNLP.SIF import sif4sci, link_formulas
 # print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "return_type": "ast"}}))
 # print(sif4sci(item, tokenization_params={"formula_params": {"method": "ast", "return_type": "formula"}}))
 
-e = r"$x$ 是 $y$ 那么 $y$ 和 $z$ 是什么 $x,y,z$"
+# e = r"$x$ 是 $y$ 那么 $y$ 和 $z$ 是什么 $x,y,z$"
 # print(sif4sci(e, symbol="gm",
 #               tokenization_params={
 #                   "formula_params": {
@@ -46,6 +46,56 @@ e = r"$x$ 是 $y$ 那么 $y$ 和 $z$ 是什么 $x,y,z$"
 # ]
 # link_formulas(*tls)
 # print(tls)
-seg = sif4sci(e, tokenization=False)
-with seg.filter(keep="t"):
-    print(seg)
+# seg = sif4sci(e, tokenization=False)
+# with seg.filter(keep="t"):
+#     print(seg)
+# e = r'某校一个课外学习小组为研究某作物的发芽率y和温度x（单位：$^{\circ} \mathrm{C}$）的关系，在20个不同温度条件下进行种子发芽实验，由实验数据$\left(x_{i}, y_{i}\right)(i=1,2, \cdots, 20)$得到下面的散点图：由此散点图，在10$^{\circ} \mathrm{C}$至40$^{\circ} \mathrm{C}$之间，下面四个回归方程类型中最适宜作为发芽率y和温度x的回归方程类型的是$\FigureID{3bf20b91-8af1-11eb-86ff-b46bfc50aa29}$$\FigureID{59b851d3-8af1-11eb-bd45-b46bfc50aa29}$$\FigureID{6310d375-8b75-11eb-bf70-b46bfc50aa29}$$\FigureID{6a006175-8b76-11eb-aa57-b46bfc50aa29}$$\FigureID{088f15e7-8b7c-11eb-a8aa-b46bfc50aa29}$'
+# # e = r"$x$ 是 $y$ 那么 $y$ 和 $z$ 是什么 $x,y,z$"
+
+# e = r'已知集合$A=\left\{x \mid x^{2}-3 x-4<0\right\}, \quad B=\{-4,1,3,5\}, \quad$ 则 $A \cap B=$'
+
+from EduNLP.utils import dict2str4sif
+test_item_1 = {
+    "stem": r"若$x=2$, $y=\sqrt{x}$，则下列说法正确的是$\SIFChoice$",
+    "options": [r"$x < y$", r"$y = x$", r"$y < x$"]
+}
+e = dict2str4sif(test_item_1, tag_mode="head", add_list_no_tag=False)
+seg = sif4sci(
+    e,
+    symbol="g",
+    tokenization_params={
+        "formula_params": {
+            "method": "ast", "return_type": "list", "ord2token": True
+        }
+    },
+    errors="raise"
+)
+print(seg.get_segments())
+#
+# import json
+# from tqdm import tqdm
+#
+#
+# def load_items():
+#     with open("../../data/OpenLUNA.json", encoding="utf-8") as f:
+#         for line in f:
+#             yield json.loads(line)
+#
+#
+# from EduNLP.SIF import sif4sci
+#
+# sif_items = []
+# for i, item in tqdm(enumerate(load_items()), "sifing"):
+#     if i > 100:
+#         break
+#     sif_item = sif4sci(
+#         item["stem"],
+#         symbol="gm",
+#         tokenization_params={"formula_params": {
+#             "method": "ast",
+#             "return_type": "list",
+#             "ord2token": True,
+#         }}
+#     )
+#     if sif_item:
+#         sif_items.append(sif_item.tokens)
