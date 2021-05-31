7d017f264a0468d1d594383c535827d024c0db38
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-31 10:44:26 +0800

    [test] fix test cases

--- tests/test_vec/test_vec.py
+++ tests/test_vec/test_vec.py
@@ -2,27 +2,16 @@
 # 2021/5/30 @ tongshiwei
 
 import pytest
-from EduNLP.Pretrain import train_vector
+from EduNLP.Pretrain import train_vector, GensimWordTokenizer
 from EduNLP.Vector import W2V
-from EduNLP.SIF import sif4sci
 
 
 @pytest.fixture(scope="module")
 def stem_data(data):
     _data = []
+    tokenizer = GensimWordTokenizer()
     for e in data[:10]:
-        d = sif4sci(
-            e["stem"],
-            symbol="gm",
-            tokenization_params={
-                "formula_params": {
-                    "method": "ast",
-                    "return_type": "list",
-                    "ord2token": True
-                }
-            },
-            errors="ignore"
-        )
+        d = tokenizer(e["stem"])
         if d is not None:
             _data.append(d.tokens)
     assert _data
@@ -31,7 +20,7 @@ def stem_data(data):
 
 @pytest.mark.parametrize("method", ["sg", "cbow"])
 def test_w2v(stem_data, tmpdir, method):
-    filepath_prefix = tmpdir.mkdir(method).join("stem_tf_")
+    filepath_prefix = str(tmpdir.mkdir(method).join("stem_tf_"))
     filepath = train_vector(
         stem_data,
         filepath_prefix,
@@ -40,12 +29,12 @@ def test_w2v(stem_data, tmpdir, method):
     )
     w2v = W2V(filepath)
     w2v(stem_data[0])
-    assert len(w2v[["FIGURE"]]) == 10
+    assert len(w2v["[FIGURE]"]) == 10
 
 
 def test_fasttext(stem_data, tmpdir):
     method = "fasttext"
-    filepath_prefix = tmpdir.mkdir(method).join("stem_tf_")
+    filepath_prefix = str(tmpdir.mkdir(method).join("stem_tf_"))
     filepath = train_vector(
         stem_data,
         filepath_prefix,
@@ -54,4 +43,4 @@ def test_fasttext(stem_data, tmpdir):
     )
     w2v = W2V(filepath, fasttext=True)
     w2v(stem_data[0])
-    assert len(w2v[["FIGURE"]]) == 10
+    assert len(w2v["[FIGURE]"]) == 10
