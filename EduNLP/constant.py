5a535b12219ed50972923a49010883ccac56fc64
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-02 20:03:45 +0800

    [feat] add constant

--- EduNLP/constant.py
+++ EduNLP/constant.py
@@ -1,4 +1,8 @@
 # coding: utf-8
 # 2021/8/1 @ tongshiwei
 
-ROOT = "~/.EduNLP"
+import os
+from os.path import expanduser, join
+
+ROOT = os.environ.get("EDUNLPPATH", join(expanduser("~"), ".EduNLP"))
+MODEL_DIR = os.environ.get("EDUNLPMODELPATH", join(ROOT, "model"))
