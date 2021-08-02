56d8620dd8b450e56b5073b811bf07dd59735b10
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-02 20:10:27 +0800

    [feat] add get_pretrained_t2v methods to get pretrained t2v models

--- EduNLP/Vector/t2v.py
+++ EduNLP/Vector/t2v.py
@@ -1,9 +1,13 @@
 # coding: utf-8
 # 2021/7/13 @ tongshiwei
 
+import os
+from longling import path_append
+from EduData import get_data
 from .rnn import RNNModel
 from .gensim_vec import W2V, D2V
 from .meta import Vector
+from EduNLP.constant import MODEL_DIR
 
 MODELS = {
     "w2v": W2V,
@@ -30,3 +34,24 @@ class T2V(object):
     @property
     def vector_size(self) -> int:
         return self.i2v.vector_size
+
+
+PRETRAINED_MODELS = {
+    "d2v_all_256": ["http://base.ustc.edu.cn/data/model_zoo/EduNLP/d2v/general_all_256.zip", "d2v"],
+    "d2v_sci_256": ["http://base.ustc.edu.cn/data/model_zoo/EduNLP/d2v/general_science_256.zip", "d2v"],
+    "d2v_eng_256": ["http://base.ustc.edu.cn/data/model_zoo/EduNLP/d2v/general_english_256.zip", "d2v"],
+    "d2v_lit_256": ["http://base.ustc.edu.cn/data/model_zoo/EduNLP/d2v/general_literal_256.zip", "d2v"],
+}
+
+
+def get_pretrained_t2v(name, model_dir=MODEL_DIR):
+    if name not in PRETRAINED_MODELS:
+        raise KeyError(
+            "Unknown pretrained model %s, use one of the provided pretrained models: %s" % (
+                name, ", ".join(PRETRAINED_MODELS.keys()))
+        )
+    url, model_name, *args = PRETRAINED_MODELS[name]
+    model_path = get_data(url, model_dir)
+    if model_name in ["d2v"]:
+        model_path = path_append(model_path, os.path.basename(model_path) + ".bin", to_str=True)
+    return T2V(model_name, model_path, *args)
