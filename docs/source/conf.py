333e45533dd70ee8dbd4f246f353cbcde0cc8e0b
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-08 21:08:42 +0800

    [chore] update _static path and thumbnail

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ extensions = [
 # npsphinx
 
 nbsphinx_thumbnails = {
-    'build/blitz/sif/sif': '_static/EduNLP.png',
+    'build/blitz/sif/sif': '_static/item_figure.png',
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -88,4 +88,4 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'build/asset/_static']
