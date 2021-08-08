aaca0ae8fd3544992ec1210230ded05c70aaa35c
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-08 10:06:57 +0800

    [feat] change default stopwords setting behaviour for perf the docs

--- EduNLP/SIF/tokenization/text/tokenization.py
+++ EduNLP/SIF/tokenization/text/tokenization.py
@@ -7,14 +7,14 @@ from .stopwords import DEFAULT_STOPWORDS
 jieba.setLogLevel(logging.INFO)
 
 
-def tokenize(text, granularity="word", stopwords=DEFAULT_STOPWORDS):
+def tokenize(text, granularity="word", stopwords="default"):
     """
 
     Parameters
     ----------
     text
     granularity
-    stopwords
+    stopwords: str, None or set
 
     Returns
     -------
@@ -26,6 +26,7 @@ def tokenize(text, granularity="word", stopwords=DEFAULT_STOPWORDS):
     >>> tokenize("三角函数是基本初等函数之一", granularity="char")
     ['三', '角', '函', '数', '基', '初', '函', '数']
     """
+    stopwords = DEFAULT_STOPWORDS if stopwords == "default" else stopwords
     stopwords = stopwords if stopwords is not None else {}
     if granularity == "word":
         return [token for token in jieba.cut(text) if token not in stopwords and token.strip()]
