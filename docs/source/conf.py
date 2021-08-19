d4cd37c5671b61eef1b7b4292834f8dd99b5c722
BAOOOOOM <82091145+BAOOOOOM@users.noreply.github.com>
2021-08-19 09:39:16 +0800

    Create conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,8 +61,8 @@ nbsphinx_thumbnails = {
     'build/blitz/formula/formula.ipynb': '_static/formula.png',
     'build/blitz/sif/sif_addition.ipynb': '_static/sif_addition.png',
     'build/blitz/tokenizer/tokenizer': '_static/tokenizer.png',
-    'build/blitz/sif/sif': '_static/item_figure.png',
-    'build/blitz/sif/sif': '_static/item_figure.png',
+    'build/blitz/pretrain/prepare_dataset.ipynb': '_static/item_figure.png',
+    'build/blitz/pretrain/d2v.ipynb': '_static/item_figure.png',
 }
 
 # Add any paths that contain templates here, relative to this directory.
