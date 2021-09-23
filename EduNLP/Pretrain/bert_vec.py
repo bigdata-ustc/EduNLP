096be3b68c87d5c7d32ceaf763ceb5d0d1961cdc
nnnyt <793313994@qq.com>
2021-09-23 21:38:10 +0800

    [feat] add parameters for finetuning

--- EduNLP/Pretrain/bert_vec.py
+++ EduNLP/Pretrain/bert_vec.py
@@ -56,9 +56,6 @@ class BertTokenizer(object):
         item = ''.join(next(self.pure_text_tokenizer([item])))
         return self.tokenizer.tokenize(item)
 
-    def __len__(self):
-        return len(self.tokenizer)
-
 
 class FinetuneDataset(Dataset):
     def __init__(self, items):
@@ -76,15 +73,18 @@ def finetune_bert(items, output_dir, pretrain_model="bert-base-chinese", train_p
     model = AutoModelForMaskedLM.from_pretrained(pretrain_model)
     tokenizer = BertTokenizer(pretrain_model)
     # resize embedding for additional sepecial tokens
-    model.resize_token_embeddings(len(tokenizer))
+    model.resize_token_embeddings(len(tokenizer.tokenizer))
 
     # training parameters
     if train_params:
         mlm_probability = train_params['mlm_probability'] if 'mlm_probability' in train_params else 0.15
         epochs = train_params['epochs'] if 'epochs' in train_params else 1
         batch_size = train_params['batch_size'] if 'batch_size' in train_params else 64
-        save_steps = train_params['save_steps'] if 'save_steps' in train_params else 1000
+        save_steps = train_params['save_steps'] if 'save_steps' in train_params else 100
         save_total_limit = train_params['save_total_limit'] if 'save_total_limit' in train_params else 2
+        logging_steps = train_params['logging_steps'] if 'logging_steps' in train_params else 5
+        gradient_accumulation_steps = train_params['gradient_accumulation_steps'] \
+            if 'gradient_accumulation_steps' in train_params else 1
     else:
         # default
         mlm_probability = 0.15
@@ -92,6 +92,8 @@ def finetune_bert(items, output_dir, pretrain_model="bert-base-chinese", train_p
         batch_size = 64
         save_steps = 1000
         save_total_limit = 2
+        logging_steps = 5
+        gradient_accumulation_steps = 1
 
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer.tokenizer, mlm=True, mlm_probability=mlm_probability
@@ -106,13 +108,15 @@ def finetune_bert(items, output_dir, pretrain_model="bert-base-chinese", train_p
         per_device_train_batch_size=batch_size,
         save_steps=save_steps,
         save_total_limit=save_total_limit,
-        prediction_loss_only=True,
+        logging_steps=logging_steps,
+        gradient_accumulation_steps=gradient_accumulation_steps,
     )
 
     trainer = Trainer(
         model=model,
         args=training_args,
         data_collator=data_collator,
+        tokenizer=tokenizer.tokenizer,
         train_dataset=dataset,
     )
     trainer.train()
