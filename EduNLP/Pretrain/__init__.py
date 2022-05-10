bc4b2f9bc818832560f247b2e1f52022fbe469f6
KenelmQLH <46173279+KenelmQLH@users.noreply.github.com>
2022-05-10 11:35:50 +0800

    Revert "[FEATURE] Add Elmo" (#119)

--- EduNLP/Pretrain/__init__.py
+++ EduNLP/Pretrain/__init__.py
@@ -3,4 +3,3 @@
 
 from .gensim_vec import train_vector, GensimWordTokenizer, GensimSegTokenizer
 from .bert_vec import BertTokenizer, finetune_bert
-from .elmo_vec import ElmoTokenizer, ElmoDataset, train_elmo
