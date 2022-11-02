365152706bd345f0ffa81ea7c629fc5d4286daba
nnnyt <793313994@qq.com>
2022-11-02 14:05:44 +0800

    [test] remove deprecated tests

--- tests/test_vec/conftest.py
+++ tests/test_vec/conftest.py
@@ -15,33 +15,3 @@ def data():
         for line in f.readlines():
             _data.append(json.loads(line))
     return _data
-
-
-@pytest.fixture(scope="module")
-def disen_train_data():
-    _data = []
-    data_path = path_append(abs_current_dir(__file__), "../../static/test_data/disenq_train.json", to_str=True)
-    with codecs.open(data_path, encoding="utf-8") as f:
-        for line in f.readlines():
-            _data.append(json.loads(line))
-    return _data
-
-
-@pytest.fixture(scope="module")
-def disen_test_data():
-    _data = []
-    data_path = path_append(abs_current_dir(__file__), "../../static/test_data/disenq_test.json", to_str=True)
-    with codecs.open(data_path, encoding="utf-8") as f:
-        for line in f.readlines():
-            _data.append(json.loads(line))
-    return _data
-
-
-@pytest.fixture(scope="module")
-def quesnet_data():
-    _data = []
-    data_path = path_append(abs_current_dir(__file__), "../../static/test_data/quesnet_data.json", to_str=True)
-    with codecs.open(data_path, encoding="utf-8") as f:
-        for line in f.readlines():
-            _data.append(json.loads(line))
-    return _data
