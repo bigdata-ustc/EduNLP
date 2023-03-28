99ed226a2ce057aa39337e8aa2da2ff458d71867
nnnyt <793313994@qq.com>
2023-03-28 10:45:15 +0800

    add finetuning for disenq

--- EduNLP/Pretrain/__init__.py
+++ EduNLP/Pretrain/__init__.py
@@ -5,6 +5,6 @@ from .gensim_vec import train_vector, GensimWordTokenizer, GensimSegTokenizer
 from .elmo_vec import *
 from .bert_vec import *
 from .quesnet_vec import QuesNetTokenizer, pretrain_quesnet, Question
-from .disenqnet_vec import DisenQTokenizer, train_disenqnet
+from .disenqnet_vec import *
 from .pretrian_utils import *
 from .hugginface_utils import *
